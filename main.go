d6cb740f2750eba01b369af7792228354ab94e1b
Bruce Marriner <bruce@sqls.net>
2016-08-17 07:30:01 -0500

    Too many comments.

--- main.go
+++ main.go
@@ -1,5 +1,8 @@
+// Declare this file to be part of the main package so it can be compiled into
+// an executable.
 package main
 
+// Import all Go packages required for this file.
 import (
 	"flag"
 	"fmt"
@@ -11,15 +14,19 @@ import (
 	"github.com/bwmarrin/discordgo"
 )
 
+// Declare a constant that stores the Disgord version information.
 const Version = "v0.0.0-alpha"
 
-// Global DiscordGo Session
+// Declare a DiscordGo Session in the global space so it can be easily used
+// throughout this program.
 // In this use case, there is no error that would be returned.
 var Session, _ = discordgo.New()
 
+// Read it all configuration options from both environment variables and
+// command line arguments.
 func init() {
 
-	// Get Bot Token
+	// Discord Authentication Token
 	Session.Token = os.Getenv("DG_TOKEN")
 	if Session.Token == "" {
 		flag.StringVar(&Session.Token, "t", "", "Discord Authentication Token")
@@ -28,6 +35,10 @@ func init() {
 
 func main() {
 
+	// Declare any variables needed later.
+	var err error
+
+	// Print out a fancy logo!
 	fmt.Printf(` 
 	________  .__                               .___
 	\______ \ |__| ______ ____   ___________  __| _/
@@ -39,18 +50,17 @@ func main() {
 	// Parse command line arguments
 	flag.Parse()
 
-	// Connect to Discord if a Token was provided.
+	// Verify a Token was provided
 	if Session.Token == "" {
 		log.Println("You must provide a Discord authentication token.")
 		return
 	}
 
-	// Verify the Token and grab user information
-	u, err := Session.User("@me")
+	// Verify the Token is valid and grab user information
+	Session.State.User, err = Session.User("@me")
 	if err != nil {
 		log.Printf("error fetching user information, %s\n", err)
 	}
-	Session.State.User = u
 
 	// Open a websocket connection to Discord
 	err = Session.Open()
@@ -58,6 +68,7 @@ func main() {
 		log.Printf("error opening connection to Discord, %s\n", err)
 	}
 
+	// Wait for a CTRL-C
 	log.Printf(`Now running. Press CTRL-C to exit.`)
 	sc := make(chan os.Signal, 1)
 	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
