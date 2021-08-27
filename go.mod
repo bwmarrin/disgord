8c246e7b4fe7d7d94a938b88335c8b635d030fea
Vic Bancroft <vic.bancroft@gmail.com>
2021-08-27 17:00:22 -0400

    version update

--- go.mod
+++ go.mod
@@ -1,5 +1,8 @@
-module github.com/bwmarrin/disgord
+module github.com/oommoo/disgord
 
 go 1.13
 
-require github.com/bwmarrin/discordgo v0.19.0
+require (
+	github.com/bwmarrin/discordgo v0.19.0
+	github.com/bwmarrin/disgord v0.0.0-20200407171809-1fe97f20c0de
+)
--- main.go
+++ main.go
@@ -15,7 +15,7 @@ import (
 )
 
 // Version is a constant that stores the Disgord version information.
-const Version = "v0.0.0-alpha"
+const Version = "v0.0.0-beta"
 
 // Session is declared in the global space so it can be easily used
 // throughout this program.
